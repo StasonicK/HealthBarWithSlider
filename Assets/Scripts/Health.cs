4983e6d581d0f0e571bc81207180e1f7dd1348a9
Anishchenko Stanislav <anischenko_stas@mail.ru>
2022-05-22 20:35:41 +0500

    Refactored code

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -8,58 +8,16 @@ using UnityEngine.UI;
 public class Health : MonoBehaviour
 {
     [SerializeField] private Slider _slider;
-    [SerializeField] private float _timeStep = 0.5f;
-
-    private const string  PersonDead = "Персонаж умер";
-    private const string  PersonFullHealth = "Персонаж имеет максимум здоровья";
-    public event UnityAction<string> HealthText;
-    private Coroutine _job;
-    public float CurrentHealth => _slider.value;
+    
+    public event UnityAction<float> ChangedHealth;
 
     public void TakeDamage()
     {
-        if (_job != null)
-        {
-            StopCoroutine(_job);
-        }
-
-        _job = StartCoroutine(ChangeHealth(_slider.value - 10f));
+        ChangedHealth?.Invoke( _slider.value - 10f);
     }
 
     public void Heal()
     {
-        if (_job != null)
-        {
-            StopCoroutine(_job);
-        }
-
-        _job = StartCoroutine(ChangeHealth(_slider.value + 10f));
-    }
-
-    private IEnumerator ChangeHealth(float targetHP)
-    {
-        string healthText;
-        var waitForOneSeconds = new WaitForSeconds(_timeStep);
-        
-        while (_slider.value != targetHP)
-        {
-            _slider.value = Mathf.MoveTowards(_slider.value, targetHP, 1f);
-            healthText = $"{_slider.value} HP";
-
-            if (_slider.value == _slider.maxValue)
-            {
-                HealthText?.Invoke(PersonFullHealth);
-            }
-            else if (_slider.value == _slider.minValue)
-            {
-                HealthText?.Invoke(PersonDead);
-            }
-            else
-            {
-                HealthText?.Invoke(healthText);
-            }
-
-            yield return waitForOneSeconds;
-        }
+        ChangedHealth?.Invoke( _slider.value + 10f);
     }
 }
--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -7,26 +7,62 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
+    [SerializeField] private Slider _slider;
     [SerializeField] private TextMeshProUGUI _text;
     [SerializeField] private Health _health;
+    [SerializeField] private float _timeStep = 0.5f;
+
+    private const string PersonDead = "Персонаж умер";
+    private const string PersonFullHealth = "Персонаж имеет максимум здоровья";
+    private Coroutine _job;
 
     private void OnEnable()
     {
-        _health.HealthText += ChangeHealth;
+        _health.ChangedHealth += ChangeChangedHealth;
     }
 
     private void OnDisable()
     {
-        _health.HealthText -= ChangeHealth;
+        _health.ChangedHealth -= ChangeChangedHealth;
     }
 
     private void Start()
     {
-        _text.text = $"{_health.CurrentHealth} HP";
+        _text.text = $"{_slider.value} HP";
     }
 
-    private void ChangeHealth(string healthText)
+    private void ChangeChangedHealth(float targetHP)
     {
-        _text.text = healthText;
+        if (_job != null)
+        {
+            StopCoroutine(_job);
+        }
+
+        _job = StartCoroutine(DoChangeHealth(targetHP));
+    }
+
+    private IEnumerator DoChangeHealth(float targetHP)
+    {
+        var waitForOneSeconds = new WaitForSeconds(_timeStep);
+
+        while (_slider.value != targetHP)
+        {
+            _slider.value = Mathf.MoveTowards(_slider.value, targetHP, 1f);
+
+            if (_slider.value == _slider.maxValue)
+            {
+                _text.text = PersonFullHealth;
+            }
+            else if (_slider.value == _slider.minValue)
+            {
+                _text.text = PersonDead;
+            }
+            else
+            {
+                _text.text = $"{_slider.value} HP";
+            }
+
+            yield return waitForOneSeconds;
+        }
     }
 }
