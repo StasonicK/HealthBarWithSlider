4c71ba726534f41e7d511b0a9ea20b27c0b42bb1
Anishchenko Stanislav <anischenko_stas@mail.ru>
2022-05-22 18:01:36 +0500

    Refactored code

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -7,15 +7,70 @@ using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
-    public event UnityAction<float> IsHealed;
+    [SerializeField] private Slider _slider;
+    [SerializeField] private float _timeStep = 0.5f;
+
+    private const string  PersonDead = "Персонаж умер";
+    private const string  PersonFullHealth = "Персонаж имеет максимум здоровья";
+    public event UnityAction<string> HealthText;
+    private Coroutine _job;
+    public float CurrentHealth => _slider.value;
 
     public void TakeDamage()
     {
-        IsHealed?.Invoke(-10f);
+        if (_job != null)
+        {
+            StopCoroutine(_job);
+        }
+
+        _job = StartCoroutine(ChangeHealth(_slider.value - 10f));
     }
 
     public void Heal()
     {
-        IsHealed?.Invoke(10f);
+        if (_job != null)
+        {
+            StopCoroutine(_job);
+        }
+
+        _job = StartCoroutine(ChangeHealth(_slider.value + 10f));
+    }
+
+    private IEnumerator ChangeHealth(float targetHP)
+    {
+        string healthText;
+        var waitForOneSeconds = new WaitForSeconds(_timeStep);
+        
+        while (_slider.value != targetHP)
+        {
+            _slider.value = Mathf.MoveTowards(_slider.value, targetHP, 1f);
+            healthText = $"{_slider.value} HP";
+
+            if (_slider.value == _slider.maxValue)
+            {
+                HealthText?.Invoke(PersonFullHealth);
+            }
+            else if (_slider.value == _slider.minValue)
+            {
+                HealthText?.Invoke(PersonDead);
+            }
+            else
+            {
+                HealthText?.Invoke(healthText);
+            }
+
+            yield return waitForOneSeconds;
+        }
+
+        // if (_slider.value == _slider.maxValue)
+        // {
+        //     HealthText?.Invoke(PersonFullHealth);
+        //     yield return waitForOneSeconds;
+        // }
+        // else if (_slider.value == _slider.minValue)
+        // {
+        //     HealthText?.Invoke(PersonDead);
+        //     yield return waitForOneSeconds;
+        // }
     }
 }
